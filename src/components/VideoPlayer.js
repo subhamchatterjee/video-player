a1e6cd1db61aac6fc2ac6fc2b4bc037e35f326f7
Subham Chatterjee <subham.b.chatterjee@gmail.com>
2022-07-25 00:27:10 +0530

    Updated error and loader

--- src/components/VideoPlayer.js
+++ src/components/VideoPlayer.js
@@ -1,34 +1,61 @@
 import React, { useEffect, useState, useRef } from 'react';
+import logo from '../logo.svg';
 
 const VideoPlayer = () => {
+  const SEEK_SECONDS = 5;
   const videoRef = useRef();
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    fetch("http://localhost:8000/video")
-    .then((res) => res.json())
-    .then((data) => {
-      setLoading(false);
-      if(data?.status && data?.url && videoRef?.current) {
-        videoRef.current.src = data.url;
-      }
-    }).catch((err) => {
-      console.log("error in fetching video url", err);
-    });
-  }, []);
+  const [error, setError] = useState(null);
+  const [videoUrl, setVideoUrl] = useState(null);
 
   const seek = (type) => {
     let currentTime = videoRef?.current?.currentTime || 0;
     if(type === "forward") {
-      videoRef.current.currentTime = currentTime + 5;
+      videoRef.current.currentTime = currentTime + SEEK_SECONDS;
     } else if(type === "backward") {
-      videoRef.current.currentTime = currentTime - 5;
+      videoRef.current.currentTime = currentTime - SEEK_SECONDS;
     }
+  }
+
+  const showError = () => {
+    setError("Video could not be loaded! Please try refreshing the page again.");
+  }
+
+  const getVideoUrl = () => {
+    fetch("http://localhost:8000/video")
+    .then((res) => res.json())
+    .then((data) => {
+      if(data?.success && data?.url) {
+        setVideoUrl(data.url);
+      } else {
+        showError();
+      }
+    }).catch((err) => {
+      console.log("error in fetching video url", err);
+      showError();
+    });
+  }
+
+  useEffect(() => {
+    if(videoUrl && videoRef?.current) {
+      videoRef.current.src = videoUrl;
+    }
+  }, [videoUrl]);
+
+  useEffect(() => {
+    getVideoUrl();
+  }, []);
+
+  if(error) {
+    return (
+      <div className="video-player-container">
+        <div className="error">{error}</div>
+      </div>
+    )
   }
 
   return (
     <div className="video-player-container">
-      {!loading ? (
+      {videoUrl ? (
         <div className="video-player">
           <div className="arrow arrow-left" onClick={() => seek("backward")} title="5 secs backward">&#8249;</div>
           <video ref={videoRef} controls width="100%" />
@@ -36,7 +63,8 @@ const VideoPlayer = () => {
         </div>
       ) : (
         <div className="loading-video">
-          Loading...
+          Loading
+          <img src={logo} className="loader" alt="loader" />
         </div>
       )}
     </div>
